33eada26e08477a89ca7568452fc5525f204f836
Mike Rose <michaelrose151@gmail.com>
2023-02-27 16:46:07 +0000

    added new and create methods, and a new page, for reptiles

--- app/controllers/reptiles_controller.rb
+++ app/controllers/reptiles_controller.rb
@@ -2,4 +2,23 @@ class ReptilesController < ApplicationController
   def index
     @reptiles = Reptile.all
   end
+
+  def new
+    @reptile = Reptile.new
+  end
+
+  def create
+    @reptile = Reptile.new(reptile_params)
+    if @reptile.save
+      redirect_to reptile_path(@reptile)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def reptile_params
+    params.require(:reptile).permit(:name, :description, :species, :daily_price, :user_id)
+  end
 end
