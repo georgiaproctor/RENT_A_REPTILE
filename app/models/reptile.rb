75f5f9e6c5d3b7fe836f098b53a8cfcc442153eb
Georgia Proctor <georgiaproctors@gmail.com>
2023-02-28 14:14:04 +0000

    added validations to reptile model + changed reptiles table daily_price from integer to float

--- app/models/reptile.rb
+++ app/models/reptile.rb
@@ -1,3 +1,7 @@
 class Reptile < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true, length: { minimum: 100, maximum: 500 }
+  validates :species, presence: true, length: { minimum: 1, maximum: 100 }
+  validates :daily_price, presence: true, numericality: true
 end
