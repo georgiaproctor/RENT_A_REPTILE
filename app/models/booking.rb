0ef65185db84c799be7e79a07b0c1abb1e6129de
Mike Rose <michaelrose151@gmail.com>
2023-02-28 16:22:29 +0000

    added booking validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :reptile
+  validates :start_date, presence: true
+  validates :end_date, presence: true, date: { after_or_equal_to: :start_date }
 end
