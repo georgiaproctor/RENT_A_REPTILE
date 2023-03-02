a13391ede16c291b7524f4a60ef824a444fa2ca9
Georgia Proctor <georgiaproctors@gmail.com>
2023-03-02 17:35:59 +0000

    edited the validations so the create reptile listing feature works

--- app/models/reptile.rb
+++ app/models/reptile.rb
@@ -1,8 +1,8 @@
 class Reptile < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
-  validates :description, presence: true, length: { minimum: 1, maximum: 1500 }
-  validates :species, presence: true, length: { minimum: 1, maximum: 100 }
+  validates :description, presence: true
+  validates :species, presence: true
   validates :daily_price, presence: true, numericality: true
 
   has_one_attached :photo
